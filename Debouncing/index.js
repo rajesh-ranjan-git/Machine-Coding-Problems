e45b2201b289506a2783829dfdaf6583db346423
Rajesh <rajeshranjan8271@gmail.com>
2024-07-20 03:54:11 +0530

    Debouncing

---
+++ Debouncing/index.js
@@ -0,0 +1,16 @@
+const input = document.querySelector(".input");
+
+const inputHandler = (event) => {
+  console.log(event.target.value);
+};
+
+const debounce = (callback, delay) => {
+  let timerID;
+  return (...args) => {
+    clearTimeout(timerID);
+    timerID = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
+input.addEventListener("keyup", debounce(inputHandler, 500));
